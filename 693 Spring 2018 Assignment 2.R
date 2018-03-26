9fdf6c91d7e5e8e17e7619ffc1e0117576a7d9e0
tges222 <35349246+tges222@users.noreply.github.com>
2018-03-26 15:14:32 -0400

    first
    working

--- 693 Spring 2018 Assignment 2.R
+++ 693 Spring 2018 Assignment 2.R
@@ -8,3 +8,48 @@ library(nycflights13)
 
 ## dataset for assignment
 flights
+
+
+
+##### 3
+cancel.flights=flights %>% 
+  group_by(month,day) %>% 
+  summarise(avg_delay=mean(dep_delay,na.rm=T),cancel.prop=sum(is.na(air_time))/length(air_time))
+
+anova(lm(avg_delay~cancel.prop,data=cancel.flights))
+
+
+##### 4
+delays=flights %>% 
+  group_by(carrier) %>% 
+  summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
+  arrange(avg_delay)
+
+#challenge
+flights %>% 
+  group_by(carrier,dest) %>% 
+  summarise(n())
+
+anova(lm(dep_delay~carrier+origin,data=flights))
+
+
+##### 5
+flights %>%
+  arrange(tailnum,month,day)
+
+
+##### 6
+flights %>% 
+  group_by(tailnum) %>%
+  summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
+  arrange(avg_delay)
+  
+
+##### 7
+flights %>% 
+  group_by(sched_dep_time) %>%
+  summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
+  arrange(avg_delay)
+
+
+
