9ec737704a1db6bbccd5c4e201c06079a9c59d64
tges222 <35349246+tges222@users.noreply.github.com>
2018-03-26 17:06:04 -0400

    Day One

--- 693 Spring 2018 Assignment 2.R
+++ 693 Spring 2018 Assignment 2.R
@@ -47,9 +47,44 @@ flights %>%
 
 ##### 7
 flights %>% 
-  group_by(sched_dep_time) %>%
+  group_by(hour) %>%
   summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
   arrange(avg_delay)
 
 
+##### 8
+flights %>% 
+  group_by(dest) %>% 
+  summarise(total.delay=sum(dep_delay,na.rm=T))
+
+
+#??
+flights %>% 
+  group_by(dest,carrier) %>% 
+  summarise(total.delay=sum(dep_delay,na.rm=T)) %>% 
+  arrange(carrier)
+
+
+##### 9
+#suspiciously fast
+fast=flights %>% 
+  #mutate(speed=distance/air_time) %>% 
+  group_by(dest,origin) %>% 
+  #mutate(avg_speed=mean(speed,na.rm=T)) %>% 
+  mutate(avg_air=mean(air_time,na.rm=T)) %>% 
+  #mutate(fast=speed/avg_speed) %>% 
+  mutate(fast=air_time/avg_air) %>% 
+  arrange(fast) %>% 
+  #select(fast,speed,avg_speed,origin,dest,distance,carrier,flight,air_time)
+  select(fast,air_time,avg_air,origin,dest,distance,carrier,flight)
+
+  
+
+
+
+#most delayed flights in the air
+flights %>% 
+  select(sched_dep_time,sched_arr_time,air_time,origin,dest,arr_delay,dep_delay) %>% 
+  mutate(air_delay=arr_delay-dep_delay) %>% 
+  arrange(desc(air_delay))
 
