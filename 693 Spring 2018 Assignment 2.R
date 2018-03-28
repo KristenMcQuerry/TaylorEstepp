96c967a9b9e33713ff9b411d31a79cfe32956bfe
tges222 <35349246+tges222@users.noreply.github.com>
2018-03-28 11:59:12 -0400

    finished
    all except the one we cant do

--- 693 Spring 2018 Assignment 2.R
+++ 693 Spring 2018 Assignment 2.R
@@ -1,3 +1,6 @@
+#taylor estepp
+#practicum assignment 2
+
 ##install packages
 install.packages("tidyverse")
 install.packages("nycflights13")
@@ -12,18 +15,28 @@ flights
 
 
 ##### 3
+#number of canceled flights per day
+can=flights %>% 
+  group_by(month,day) %>% 
+  summarise(num_cancel=sum(is.na(air_time)))
+#pattern
+ggplot(can,aes(x=day,y=num_cancel,color=month))+
+  geom_point()
+#is proportion of cancelled flights related to the average delay
 cancel.flights=flights %>% 
   group_by(month,day) %>% 
   summarise(avg_delay=mean(dep_delay,na.rm=T),cancel.prop=sum(is.na(air_time))/length(air_time))
 
 anova(lm(avg_delay~cancel.prop,data=cancel.flights))
+#cancel proportion related to avg delay
 
 
 ##### 4
+#which carrier has worst delays
 delays=flights %>% 
   group_by(carrier) %>% 
   summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
-  arrange(avg_delay)
+  arrange(desc(avg_delay))
 
 #challenge
 flights %>% 
@@ -34,18 +47,23 @@ anova(lm(dep_delay~carrier+origin,data=flights))
 
 
 ##### 5
+#number of flights before first delay >60 NOT DONE
 flights %>%
-  arrange(tailnum,month,day)
+  arrange(tailnum,month,day) 
+#i dont know how to iteratively add rows of a tibble until a condition is met
+#or filter out all of the rows after a condition is met
 
 
 ##### 6
+#tailnum with worst on time record
 flights %>% 
   group_by(tailnum) %>%
   summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
-  arrange(avg_delay)
+  arrange(desc(avg_delay))
   
 
 ##### 7
+#what time should you fly to avoid delays
 flights %>% 
   group_by(hour) %>%
   summarise(avg_delay=mean(dep_delay,na.rm=T)) %>% 
@@ -53,38 +71,33 @@ flights %>%
 
 
 ##### 8
+#total minutes of delay per destination
 flights %>% 
   group_by(dest) %>% 
   summarise(total.delay=sum(dep_delay,na.rm=T))
-
-
-#??
+#compute prop of total delay for each row
 flights %>% 
-  group_by(dest,carrier) %>% 
-  summarise(total.delay=sum(dep_delay,na.rm=T)) %>% 
-  arrange(carrier)
+  group_by(dest) %>% 
+  mutate(total.delay=sum(dep_delay,na.rm=T)) %>%
+  mutate(delay_prop=abs(dep_delay/total.delay)) %>% 
+  select(total.delay,carrier,dest,dep_delay,delay_prop) %>% 
+  arrange(dest)
+  
 
 
 ##### 9
 #suspiciously fast
-fast=flights %>% 
-  #mutate(speed=distance/air_time) %>% 
+flights %>% 
   group_by(dest,origin) %>% 
-  #mutate(avg_speed=mean(speed,na.rm=T)) %>% 
   mutate(avg_air=mean(air_time,na.rm=T)) %>% 
-  #mutate(fast=speed/avg_speed) %>% 
   mutate(fast=air_time/avg_air) %>% 
   arrange(fast) %>% 
-  #select(fast,speed,avg_speed,origin,dest,distance,carrier,flight,air_time)
-  select(fast,air_time,avg_air,origin,dest,distance,carrier,flight)
-
-  
-
-
+  select(fast,air_time,avg_air,origin,dest,distance,carrier)
 
 #most delayed flights in the air
 flights %>% 
-  select(sched_dep_time,sched_arr_time,air_time,origin,dest,arr_delay,dep_delay) %>% 
+  select(sched_dep_time,sched_arr_time,air_time,origin,dest,arr_delay,dep_delay,distance) %>% 
   mutate(air_delay=arr_delay-dep_delay) %>% 
-  arrange(desc(air_delay))
-
+  mutate(delay_dist=air_delay/distance) %>% #minutes per mile in the air lost
+  arrange(desc(delay_dist))
+#possibly values over 1 are error
